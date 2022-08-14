769de7e8c6a9c44ec519975cab2e43fe75d5628e
Opeyemi Adeniji <adenijiopeyemi68@gmail.com>
2022-08-14 20:13:43 +0100

    edit

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         Okiri Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <h2>okirigabriel24@gmail.com</h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
