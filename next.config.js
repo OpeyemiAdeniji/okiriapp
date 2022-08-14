0b28c07ba89cc9ab9e105d008345f8715315aff2
Opeyemi Adeniji <adenijiopeyemi68@gmail.com>
2022-08-14 20:04:32 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'imgix',
+    path: '/Users/opeyemiadeniji/Documents/my-app/public',
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 }
 
 module.exports = nextConfig
